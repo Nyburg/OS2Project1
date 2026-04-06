976cea5a790f120c56e002641492eedda6a29099
Nick Nyburg <nick@Mac-11.lan>
2026-04-05 23:06:29 -0700

    Add process struct, state enum and program parsing

--- rrsim.c
+++ rrsim.c
@@ -9,7 +9,48 @@
 
 #define MIN(x,y) ((x)<(y)?(x):(y)) // Compute the minimum
 
-// TODO: all the code!
+enum process_state {
+    READY,
+    SLEEPING,
+    EXITED
+};
+
+struct process {
+    int pid;
+    enum process_state state;
+    int sleep_time_remaining;
+    int awake_time_remaining;
+    int pc;
+    int program[MAX_PROG_LEN + 1];
+};
+
+static struct process table[MAX_PROCS];
+
+/**
+ * Parse one command-line program into a process table entry.
+ */
+void parse_program(char *arg, int pid)
+{
+    char *token;
+    int index = 0;
+
+    table[pid].pid = pid;
+    table[pid].state = READY;
+    table[pid].sleep_time_remaining = 0;
+    table[pid].awake_time_remaining = 0;
+    table[pid].pc = 0;
+
+    token = strtok(arg, ",");
+
+    while (token != NULL && index < MAX_PROG_LEN) {
+        table[pid].program[index] = atoi(token);
+        token = strtok(NULL, ",");
+        index++;
+    }
+
+    table[pid].program[index] = 0;
+    table[pid].awake_time_remaining = table[pid].program[0];
+}
 
 /**
  * Main.
@@ -17,9 +58,17 @@
 int main(int argc, char **argv)
 {
     int clock = 0;
+    int proc_count = argc - 1;
+    int i;
 
     struct queue *q = queue_new();
 
-    // TODO
+    for (i = 0; i < proc_count; i++) {
+        parse_program(argv[i + 1], i);
+        queue_enqueue(q, &table[i]);
+    }
+
+    (void) clock;
+
     queue_free(q);
 }
